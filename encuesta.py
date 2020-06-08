17aa80d856d5e0976cc3cdb1a4678c8f34567cfd
Jorge <maskechino@gmail.com>
2020-06-08 19:29:33 -0400

    Cambio de clases
    Agregar una nueva pregunta pasa a ser una clase

--- encuesta.py
+++ encuesta.py
@@ -12,34 +12,36 @@ class Encuesta:
             'preguntas' : [],
         }
 
+    def ingresar_pregunta(self):
+        continuar = 'si'
+        if continuar == 'si':
+            pregunta_respuestas = []
+            for nueva_respuesta in range(2):
+                nueva_respuesta = 'si'
+                pregunta  = input("Ingrese Pregunta:\n")
+                respuestas = []            
+                while nueva_respuesta == 'si':
+                    respuestas.append(input("ingrese su respuesta:\n"))                            
+                    nueva_respuesta = input("Desea ingresar otra respuesta?:\n")
+                    nueva_pregunta = {}
+                    nueva_pregunta[pregunta] = respuestas            
+                pregunta_respuestas.append(nueva_pregunta)
+                continuar = input("ingresar nueva pregunta?\n")
+                if continuar == "no":
+                    encuesta.n_encuesta['preguntas'] = pregunta_respuestas
+                    break
+
 encuesta = Encuesta()
 
 preguntas = encuesta.n_encuesta['preguntas']
 
-def ingresar_pregunta():
-    continuar = 'si'
-    if continuar == 'si':
-        pregunta_respuestas = []
-        for nueva_respuesta in range(4):
-            nueva_respuesta = 'si'
-            pregunta  = input("Ingrese Pregunta:\n")
-            respuestas = []
-            while nueva_respuesta == 'si':
-                respuestas.append(input("ingrese su respuesta:\n"))                            
-                nueva_respuesta = input("Desea ingresar otra respuesta?:\n")
-                nueva_pregunta = {}
-                nueva_pregunta[pregunta] = respuestas            
-            pregunta_respuestas.append(nueva_pregunta)
-            continuar = input("ingresar nueva pregunta?\n")
-            if continuar == "no":
-                encuesta.n_encuesta['preguntas'] = pregunta_respuestas
-                break
+
 
 def nueva_encuesta():
     encuesta.n_encuesta['titulo'] = input("Ingrese un titulo:\n")
     encuesta.n_encuesta['grupo'] = input("Ingrese un grupo:\n")
     encuesta.n_encuesta['vencimiento'] = input("Ingrese un vencimiento:\n")
-    ingresar_pregunta()
+    encuesta.ingresar_pregunta()
 
 agregar_encuesta = input("Desea crear una nueva encuesta?\n")
 
@@ -48,7 +50,7 @@ while agregar_encuesta == "si":
     agregar_encuesta = input("Desea crear una nueva encuesta?\n")
     listadoEncuestas.append(encuesta.n_encuesta)
 else:
-    print("\nGracias por crear una nueva encuesta")
+    print("\nGracias por crear una nueva encuesta\n")
 
 
 
